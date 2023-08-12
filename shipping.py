4bb2df96390d8f06b34a43fbbf4a9e4e23f97a51
Kateryna Berezovska <111088730+kberezov@users.noreply.github.com>
2023-08-12 11:55:53 -0400

    Shipping cost calculator for Sal's Shipping

---
+++ shipping.py
@@ -0,0 +1,61 @@
+"""Sal runs the biggest shipping company in the tri-county area, Sal’s Shippers. Sal wants to make sure that every single one of his customers has the best, and most affordable experience shipping their packages.
+In this project, you’ll build a program that will take the weight of a package and determine the cheapest way to ship that package using Sal’s Shippers.
+Sal’s Shippers has several different options for a customer to ship their package:
+
+Ground Shipping, which is a small flat charge plus a rate based on the weight of your package.
+Ground Shipping Premium, which is a much higher flat charge, but you aren’t charged for weight.
+Drone Shipping (new), which has no flat charge, but the rate based on weight is triple the rate of ground shipping.
+Here are the prices:
+
+Ground Shipping
+Weight of Package	Price per Pound	Flat Charge
+2 lb or less	$1.50	$20.00
+Over 2 lb but less than or equal to 6 lb	$3.00	$20.00
+Over 6 lb but less than or equal to 10 lb	$4.00	$20.00
+Over 10 lb	$4.75	$20.00
+
+Ground Shipping Premium
+Flat charge: $125.00
+
+Drone Shipping
+Weight of Package	Price per Pound	Flat Charge
+2 lb or less	$4.50	$0.00
+Over 2 lb but less than or equal to 6 lb	$9.00	$0.00
+Over 6 lb but less than or equal to 10 lb	$12.00	$0.00
+Over 10 lb	$14.25	$0.00"""
+
+#Ask for user input
+weight = input("Enter the weight of yout package: ")
+weight = float(weight)
+
+#Ground shipping
+if weight <= 2:
+  ground_cost = weight*1.5+20
+elif weight <= 6:
+  ground_cost = weight*3+20
+elif weight <= 10:
+  ground_cost = weight*4+20
+else:
+  ground_cost = weight*4.75+20
+print("\nThe price of Ground Shipping for you package is $%f" % ground_cost)
+
+#Ground shipping premium
+premium_cost = 125
+print("The price of Ground Shipping Premium is a flat charge of $%f" % premium_cost)
+
+#Drone Shipping
+if weight <= 2:
+  drone_cost = weight*4.5
+elif weight <= 6:
+  drone_cost = weight*9
+elif weight <= 10:
+  drone_cost = weight*12
+else:
+  drone_cost = weight*14
+print("The price of Drone Shipping for you package is $%f" % drone_cost)
+
+#Find the cheapest mode of shipping
+cheapest_cost = min(ground_cost, premium_cost, drone_cost)
+print("The cheapest shipping price for your package is $%f" %cheapest_cost)
+
+print("Goodbye")
